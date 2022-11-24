47ce91fcf8ea3ddf93d5c07702012de8a82395cc
123Asadrifat <117895290+123Asadrifat@users.noreply.github.com>
2022-11-24 14:50:36 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// ================== Nav part here =================== //
+
+var menuBtn = document.getElementById("menuBtn");
+var sideNav = document.getElementById("sideNav");
+
+sideNav.style.right = "-250px";
+menuBtn.onclick = function(){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+    }
+    else{
+        sideNav.style.right = "-250px";
+    }
+}
+
+// ================== menu click scroll part here =================== //
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+    speed: 1000,
+    speedAsDuration:true,
+});
+
+
+
+
+
+
+
+
+
+
+
